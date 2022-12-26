ce1740c527007412e9bd4be4b76d08cd725b82e3
sara_tjn <tajernia.sara@gmail.com>
2022-12-26 15:35:01 +0330

    clean code

--- q3.py
+++ q3.py
@@ -1,27 +1,23 @@
-
-"""
-Given an array of n distinct elements, find the minimum number of swaps required to sort the array.
-:param arr: list
-:return: int
-"""
 def minSwaps(arr):
-	n = len(arr)
+	"""
+	Given an array of n distinct elements, find the minimum number of swaps required to sort the array.
+	:param arr: list
+	:return: int
+	"""
 	arrpos = [*enumerate(arr)]
 	arrpos.sort(key=lambda it: it[1])
-	vis = {k: False for k in range(n)}
+	vis = {k: False for k in range(len(arr))}
 	ans = 0
-	for i in range(n):
+	for i in range(len(arr)):
 		if vis[i] or arrpos[i][0] == i:
 			continue
 
 		cycle_size = 0
 		j = i
-
 		while not vis[j]:
 			vis[j] = True
 			j = arrpos[j][0]
 			cycle_size += 1
-
 		if cycle_size > 0:
 			ans += (cycle_size - 1)
 
